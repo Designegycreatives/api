3c493f501b2df42b81756ecabf367c38a4c122ee
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-06-16 23:04:12 +0100

    Create connectDb.js

---
+++ utility/connectDb.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose"
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+}
+
+
+const connectDb = () => {
+    if (mongoose.connections[0].readyState) {
+        console.log("MongoDB already connected")
+        return
+    }
+    return mongoose.connect(process.env.MONGO_URL, options, () => {
+        console.log("connected successfully to the DB!")
+    })
+}
+
+export default connectDb
